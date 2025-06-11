ec17efce11f5f9a9788c14f4922fe18396e20a79
Ivan Martinez-Kay <martinezkayivan@gmail.com>
2025-06-11 15:07:51 -0500

    updated  nav

--- src/frontend/my-app/src/components/Navigation/Nav.js
+++ src/frontend/my-app/src/components/Navigation/Nav.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState} from "react";
+import styles from "./styles/nav.module.css"
 
 const Nav = () => {
+
   return (
-    <div>
+    
+    
+      <div className={styles.nav}>
+     
       
-    </div>
+      <header className={styles.header}>
+      <nav className={styles.nav}> 
+
+          <a href='/About'><Button2 text='About me'/></a>
+
+          <a href = '/Portfolio'><Button2 text='Portfolio'/></a>
+
+      </nav>
+      </header>  
+      </div>
   )
 }
 
 export default Nav
+
