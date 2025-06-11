04b51fcb2d67f008a05aebb2226543043d8e89db
Ivan Martinez-Kay <martinezkayivan@gmail.com>
2025-06-11 15:40:38 -0500

    new pages and routes added

--- src/frontend/my-app/src/App.js
+++ src/frontend/my-app/src/App.js
@@ -1,16 +1,29 @@
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "./components/Pages/Home";
 import Nav from "./components/Navigation/Nav";
+import Portfolio from "./components/Pages/Portfolio";
 
 function App() {
   return (
     <div className="App">
      <Router>
       <Switch>
+
         <Route exact path = "/">
             <Nav/>
             <Home/>
         </Route>
+
+        <Route exact path = "/About">
+            <Nav/>
+            <About/>
+        <Route/>
+
+        <Route exact path = "/Portfolio"></Route>
+            <Nav/>
+            <Portfolio/>
+        </Route>
+
       </Switch>
      </Router>
     </div>
