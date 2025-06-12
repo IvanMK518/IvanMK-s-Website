24daff3da0094751169c896586a67b903798d66a
Ivan Martinez-Kay <martinezkayivan@gmail.com>
2025-06-12 14:23:52 -0500

    Exp page

--- src/frontend/my-app/src/components/Pages/Experience.jsx
+++ src/frontend/my-app/src/components/Pages/Experience.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import styles from '../styles/experience.module.css'
 
 function Experience() {
   return (
-    <div>About</div>
-      
-
+    <div>
+      <div className={styles.wrapper}>
+      <div className={styles.wrapper2}>
+        <p>Test Text</p>
+      </div>
+      </div>
+    </div>
   )
 }
 
