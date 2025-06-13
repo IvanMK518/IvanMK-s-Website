42b39f555e6d8e28c52f830d05ece4616a25a02f
Ivan Martinez-Kay <martinezkayivan@gmail.com>
2025-06-13 11:24:17 -0500

    changes for deployment

--- src/frontend/my-app/src/App.js
+++ src/frontend/my-app/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { HashRouter as Router, Route, Switch} from "react-router-dom";
 import Home from "./components/Pages/Home";
 import Nav from "./components/Navigation/Nav";
 import Portfolio from "./components/Pages/Portfolio";
@@ -6,38 +6,36 @@ import Experience from "./components/Pages/Experience";
 import About from "./components/Pages/About";
 import Gallery from "./components/Pages/Gallery";
 
+
 function App() {
   return (
     <div className="App">
      <Router>
+      <Nav>
       <Switch>
 
         <Route exact path = "/">
-            <Nav/>
             <Home/>
         </Route>
 
         <Route exact path = "/Experience">
-            <Nav/>
             <Experience/>
         </Route>
 
         <Route exact path = "/Portfolio">
-            <Nav/>
             <Portfolio/>
         </Route>
 
         <Route exact path = "/About">
-            <Nav/>
             <About/>
         </Route>
         
         <Route exact path = "/Gallery">
-            <Nav/>
             <Gallery/>
         </Route>
 
       </Switch>
+      </Nav>
      </Router>
     </div>
   );
